d930593804975bf9f451e674a1702737d71007d1
Noelle Scrivener <nascrivener@gmail.com>
2024-09-19 14:53:24 -0600

    uploaded jessica

--- CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
+++ CMPUT 250 W24 Lab 3/Assets/DVDLogo.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DVDLogo : MonoBehaviour
 {
     //Speed it moves at
-    public float speed = 3;
+    private float speed = 3;
+
+    //use unity tool SpriteRenderer, create reference variable for new sprite
+    public SpriteRenderer spriteRenderer;
+    public Sprite ghostSprite;
+
+    // variable containing text for after sprite change
+    public Text afterJessica;
 
     //Bounds of the screen (could get these with camera bounds but we can do this since it's a fixed camera)
     public float X_Max = 5, Y_Max = 4;
@@ -13,12 +21,18 @@ public class DVDLogo : MonoBehaviour
     //Current direction
     private Vector3 direction;
 
+    // A function to change from Jessica to ghost sprite
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = ghostSprite;
+    }
     // Start is called before the first frame update
     void Start()
     {
         //Randomly initialize direction
         direction = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f));
         direction.Normalize();
+        //textItem.text = "New text!";
 
     }
 
@@ -46,19 +60,28 @@ public class DVDLogo : MonoBehaviour
         //See if a bounce needs to happen before moving
         if (newPosition.x>X_Max){
             FlipDirectionX();
+            ChangeSprite();
+            afterJessica.text = "Oh no. Jessica died.";
             
         }
         else if (newPosition.x<-1*X_Max){
             FlipDirectionX();
+            ChangeSprite();
+            afterJessica.text = "Oh no. Jessica died.";
         }
 
         if (newPosition.y>Y_Max){
             FlipDirectionY();
+            ChangeSprite();
+            afterJessica.text = "Oh no. Jessica died.";
         }
         else if (newPosition.y<-1*Y_Max){
             FlipDirectionY();
+            ChangeSprite();
+            afterJessica.text = "Oh no. Jessica died.";
         }
 
         transform.position += direction*Time.deltaTime*speed;
+
     }
 }
